4f35c02777fea7a9fffa6b4c53e50c4a0a7559d4
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-25 14:27:54 +0000

    added local storage

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-// Variables
+// HTML Variables
 let createNotesBtn = document.querySelector(".jsCreateNotesButton");
 let notesContainer = document.querySelector(".jsNotesContainer");
+
+// Display localStorage in notes container upon load
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
+showNotes();
+
+// Saves notes container to local storage
+function updateLocalStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 // Create note on create notes button click
 createNotesBtn.addEventListener("click", () => {
   // Create note elements
   let textContainer = document.createElement("div");
   let text = document.createElement("p");
-  let deleteNoteBtn = document.createElement("button");
   let deleteNoteImg = document.createElement("img");
 
   // Style elements
   textContainer.classList.add("textarea-container");
-  text.classList.add("note-text");
-  deleteNoteBtn.classList.add("delete-note", "jsDeleteNote");
+  text.classList.add("note-text", "jsNote");
+  deleteNoteImg.classList.add("delete-note", "jsDeleteNote");
 
   // Add attributes
   deleteNoteImg.setAttribute("src", "./images/delete.png");
   text.setAttribute("contenteditable", "true");
 
   // Append elements
-  deleteNoteBtn.appendChild(deleteNoteImg);
-  textContainer.append(text, deleteNoteBtn);
-  notesContainer.appendChild(textContainer);
+  notesContainer.appendChild(textContainer).append(text, deleteNoteImg);
+
+  updateLocalStorage();
 });
 
+// Delete relative note on delete image click
 notesContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "IMG") {
-    e.target.parentElement.parentElement.remove();
+    e.target.parentElement.remove();
+    updateLocalStorage();
+  } else if (e.target.tagName === "P") {
+    let notes = document.querySelectorAll(".jsNote");
+    notes.forEach((note) => {
+      note.addEventListener("keyup", updateLocalStorage);
+    });
+  }
+});
+
+// Inserts line break and allows local storage to display content correctly
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    e.preventDefault();
   }
 });
